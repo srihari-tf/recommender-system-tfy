01ad60e95dfafe4aa5e946d12a1f591f77a9d0fb
Srihari Radhakrishna <srihari@Sriharis-MacBook-Pro.local>
2022-11-01 21:56:32 +0530

    add comments

--- streamlit-app/deploy.py
+++ streamlit-app/deploy.py
@@ -11,8 +11,11 @@ service = Service(
     ),
     ports=[{"port": 8080}],
     env={
-        "TFY_API_KEY": 'djE6dHJ1ZWZvdW5kcnk6YWthc2g6YTQwZTIx',
+        "TFY_API_KEY": '<your-api-key>',
+        # replace with your run fqn
         "MLF_RUN_FQN": 'truefoundry/akash/movie-clustering-nov-12/cf-model'
     },
 )
+
+# replace with your workspace fqn
 deployment = service.deploy(workspace_fqn='tfy-cluster-euwe1:akash-test')
